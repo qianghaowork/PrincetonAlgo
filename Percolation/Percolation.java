de33041bba1d4753e76465e60466dd15742ba2a8
qianghaocv <qianghaowork@gmail.com>
2021-10-05 17:45:18 -0500

    Add files via upload

--- Percolation/Percolation.java
+++ Percolation/Percolation.java
@@ -1,104 +1,123 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-   private boolean[][] peropen;
-   private int top;
-   private int bottom;
-   private int size;
-   private WeightedQuickUnionUF wqu;
-   private WeightedQuickUnionUF fqu;
- 
-   // create n-by-n grid, with all sites blocked
-   public Percolation(int n) {
-       if (n <= 0) 
-          throw new IllegalArgumentException("n<=0");
-               
-       size = n;
-       top = 0;
-       bottom = n*n + 1;
-       peropen = new boolean[size][size];
-       wqu = new WeightedQuickUnionUF(n*n+2);
-       fqu = new WeightedQuickUnionUF(n*n+1);
-   }
-   
-   private int get1Dpos(int i, int j) {
-       return (i-1)*size + j;
-   }
-   
-   // open site (row i, column j) if it is not open already
-   public void open(int i, int j) {
-       if (i > 0 && i <= size && j > 0 && j <= size) {
-          if (isOpen(i, j)) {
-             return;
-          }
-          peropen[i-1][j-1] = true;
-          int npos = get1Dpos(i, j);
-          if (i == 1) {
-             wqu.union(top, npos);
-             fqu.union(top, npos);
-          }
+    private final int length;
+    private boolean[] gridFlag;
+    private boolean[] connectTop;
+    private boolean[] connectBottom;
+    private final WeightedQuickUnionUF uf;
+    private int sum;
+    private boolean percolateFlag;
 
-          if (i == size)
-             wqu.union(bottom, npos);
-          
-          if (i > 1 && isOpen(i-1, j)) {
-             wqu.union(get1Dpos(i-1, j), npos);
-             fqu.union(get1Dpos(i-1, j), npos);
-          }
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n must be positive");
+        }
+        this.length = n;
+        int size = n * n;
+        this.gridFlag = new boolean[size];
+        this.connectTop = new boolean[size];
+        this.connectBottom = new boolean[size];
+        this.uf = new WeightedQuickUnionUF(size);
+        this.sum = 0;
+    }
 
-          if (i < size && isOpen(i+1, j)) {
-             wqu.union(get1Dpos(i+1, j), npos);
-             fqu.union(get1Dpos(i+1, j), npos);
-          }
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        checkRowCol(row, col);
+        int index = getIndex(row, col);
+        if (!this.gridFlag[index]) {
+            this.gridFlag[index] = true;
+            this.sum++;
+        }
+        boolean top = false;
+        boolean bottom = false;
 
-          if (j > 1 && isOpen(i, j-1)) {
-             wqu.union(get1Dpos(i, j-1), npos);
-             fqu.union(get1Dpos(i, j-1), npos);
-          }
+        if (row < this.length && this.gridFlag[index + this.length]) {
+            if (connectTop[uf.find(index + this.length)] || connectTop[uf.find(index)]) {
+                top = true;
+            }
+            if (connectBottom[uf.find(index + this.length)] || connectBottom[uf.find(index)]) {
+                bottom = true;
+            }
+            uf.union(index, index + this.length);
+        }
 
-          if (j < size && isOpen(i, j+1)) {
-             wqu.union(get1Dpos(i, j+1), npos);
-             fqu.union(get1Dpos(i, j+1), npos);
-          }
-       }
-       else
-          throw new IndexOutOfBoundsException();
-   }
-   
-   // is site (row i, column j) open?
-   public boolean isOpen(int i, int j) {
-       if (i > 0 && i <= size && j > 0 && j <= size) { 
-         return peropen[i-1][j-1];
-       }
-       else
-         throw new IndexOutOfBoundsException();
-   }
+        if (row > 1 && this.gridFlag[index - this.length]) {
+            if (connectTop[uf.find(index - this.length)] || connectTop[uf.find(index)]) {
+                top = true;
+            }
+            if (connectBottom[uf.find(index - this.length)] || connectBottom[uf.find(index)]) {
+                bottom = true;
+            }
+            uf.union(index, index - this.length);
+        }
 
-   // is site (row i, column j) full?
-   public boolean isFull(int i, int j) {
-       if (i > 0 && i <= size && j > 0 && j <= size) {
-           return fqu.connected(top, get1Dpos(i, j));
-       }
-       else
-           throw new IndexOutOfBoundsException();
-   }
-   
-   // does the system percolate?
-   public boolean percolates() {
-       return wqu.connected(top, bottom);
-   }
-   
-   public static void main(String[] args) {
-       Percolation pc = new Percolation(5);
-       pc.open(1, 3);
-       pc.open(2, 2);
-       pc.open(2, 3);
-       System.out.println(pc.percolates());
-       pc.open(5, 4);
-       pc.open(4, 4);
-       pc.open(3, 4);
-       System.out.println(pc.percolates());
-       pc.open(3, 3);
-       System.out.println(pc.percolates());
-   }
+        if (col < this.length && this.gridFlag[index + 1]) {
+            if (connectTop[uf.find(index + 1)] || connectTop[uf.find(index)]) {
+                top = true;
+            }
+            if (connectBottom[uf.find(index + 1)] || connectBottom[uf.find(index)]) {
+                bottom = true;
+            }
+            uf.union(index, index + 1);
+        }
+
+        if (col > 1 && this.gridFlag[index - 1]) {
+            if (connectTop[uf.find(index - 1)] || connectTop[uf.find(index)]) {
+                top = true;
+            }
+            if (connectBottom[uf.find(index - 1)] || connectBottom[uf.find(index)]) {
+                bottom = true;
+            }
+            uf.union(index, index - 1);
+        }
+        if (row == 1) {
+            top = true;
+        }
+        if (row == this.length) {
+            bottom = true;
+        }
+        connectTop[uf.find(index)] = top;
+        connectBottom[uf.find(index)] = bottom;
+        if (connectTop[uf.find(index)] && connectBottom[uf.find(index)]) {
+            percolateFlag = true;
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        checkRowCol(row, col);
+        return this.gridFlag[getIndex(row, col)];
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        checkRowCol(row, col);
+        return connectTop[uf.find(getIndex(row, col))];
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        return this.sum;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return percolateFlag;
+    }
+
+    private void checkRowCol(int row, int col) {
+        if (row < 1 || row > this.length) {
+            throw new IllegalArgumentException("row is out of boundary");
+        }
+        if (col < 1 || col > this.length) {
+            throw new IllegalArgumentException("col is out of boundary");
+        }
+    }
+
+    private int getIndex(int row, int col) {
+        return (row - 1) * this.length + col - 1;
+    }
 }
